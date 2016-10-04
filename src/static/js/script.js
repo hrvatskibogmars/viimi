726bf3c26d12af574fec04d51b768c55b2030ae6
Hrga <hrvoje.orejas@gmail.com>
2016-10-04 10:20:03 +0200

    pw changes

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -102,12 +102,12 @@
     $("body").fitVids();
 
     $(".port-hov").hover(function() {
-            console.log("bok");
+            //console.log("bok");
             $(this).css('opacity', 1);
             },
             function() {
                 $(this).css('opacity', 0);
-            console.log("ajd bok");
+            //console.log("ajd bok");
 
            }
         );    
